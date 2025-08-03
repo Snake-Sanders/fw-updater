6176815d0dbafc763537e0ae3b78d6e32dc1b73a
Snake Sanders <elmarian22@gmail.com>
2025-08-03 18:03:36 +0200

    clippy use default constructor

--- src/types.rs
+++ src/types.rs
@@ -7,22 +7,13 @@ pub enum State {
     Completed, // mark update pending  and restart
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Default, Clone, PartialEq)]
 pub struct Config {
     pub addr: u32,     // 32 bits address
     pub block_num: u8, // number of blocks, 256 - control fields
     pub crc: u32,      // image flash crc
 }
 
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            addr: 0,
-            block_num: 0,
-            crc: 0,
-        }
-    }
-}
 impl Config {
     pub fn new() -> Self {
         Self::default()
