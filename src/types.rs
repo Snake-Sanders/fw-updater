e337be6e4615a526093e9cf0ad448461af9e659c
Snake Sanders <elmarian22@gmail.com>
2025-08-03 17:13:54 +0200

    add config struct

--- src/types.rs
+++ src/types.rs
@@ -6,3 +6,25 @@ pub enum State {
     Validated, // tx completed, data validated waiting to configm update
     Completed, // mark update pending  and restart
 }
+
+#[derive(Debug, PartialEq)]
+pub struct Config {
+    pub addr: u32,     // 32 bits address
+    pub block_num: u8, // number of blocks, 256 - control fields
+    pub crc: u32,      // image flash crc
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            addr: 0,
+            block_num: 0,
+            crc: 0,
+        }
+    }
+}
+impl Config {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
